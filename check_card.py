fdbca379b4d2d8834125c13089bfbbb05565f1c1
daniya_kuspanova <kyspanova@icloud.com>
2023-05-26 15:56:35 +0400

    add func check num of card

--- check_card.py
+++ check_card.py
@@ -1,23 +1,25 @@
-import hashlib
-import itertools
+from tqdm import tqdm
 
+import hashlib
 import multiprocessing as mp
 
 
-def check_num_card(default_hash: str, bin: str, main_part_card: int, last_num: str) -> bool:
-    card_num = f"{bin}{main_part_card}{last_num}"
-    if hashlib.sha3_256(card_num.encode()).hexdigest() == default_hash:
+def check_num_card(default_hash: str, bin: int, main_part_card: list, last_num: str) -> bool:
+    card_num = f"{bin}{main_part_card:06d}{last_num}"
+    tmp = hashlib.sha3_256(card_num.encode()).hexdigest()
+    if  tmp == default_hash:
         return card_num
     return False
 
-def find_num_card(default_hash: str, bins: list, last_num: str, pools: int) -> int: 
-    list_num = list(itertools.combinations_with_replacement(range(10), 7))
+def find_num_card(default_hash: str, bins: list, last_num: str, pool: int) -> int: 
+    list_num = range(1000000)
     arg = []
     for bin in bins:
         for elem in list_num:
             arg.append((default_hash, bin, elem, last_num))
-    with mp.Pool(processes=pools) as p:
-        for result in p.map(find_num_card, arg):
+    with mp.Pool(processes=pool) as p:
+        for result in p.starmap(check_num_card, tqdm(arg, colour= "#008000", 
+                                                     desc="The card number is searched by hash: ")):
             if result:
                 p.terminate()
                 return result
